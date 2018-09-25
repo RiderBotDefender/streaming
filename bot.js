56f0073d623f833e07e0be2be2d5a5ad197f2780
RiderBotDefender <41645840+RiderBotDefender@users.noreply.github.com>
2018-09-26 00:05:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,12 +4,29 @@ const client = new Discord.Client();
 
 
 
-client.on('ready', () => {
-    client.user.setGame('حياتي تعيسة ما بقى لي شي اسويه')	
+client.on('ready', function(){	
+    var ms = 40000 ;	
+    var setGame = ['اتمنى افرح من قلبي ولو مره وحدة','حياتي تعيسة مره'];	
+    var i = -1;	
+    var j = 0;	
+    setInterval(function (){	
+        if( i == -1 ){	
+j = 1;	
+       }	
+        if( i == (setGame.length)-1 ){	
+            j = -1;	
+      }	
+       i = i+j;	
+        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);	
+}, ms);	
+	
 });
 
 
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
 
 client.login(process.env.BOT_TOKEN)
